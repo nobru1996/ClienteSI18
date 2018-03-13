3bbf1c18f18c37bc67166ce2365f884b830d5d8a
arqdsis <arqdsis@mocli10220.animaedu.intranet>
2018-03-13 20:31:00 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/progmulti/clientesi18/MainActivity.java
@@ -0,0 +1,28 @@
+package br.usjt.progmulti.clientesi18;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * @author RA:816111491 Nome: Bruno Gabriel Santos Capristo Turma:SI3AN-MCA1
+ */
+public class MainActivity extends Activity {
+      public static final String NOME = "br.usjt.progmulti.clientesi18.nome";
+      private EditText txtNome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNome = findViewById(R.id.busca_nome_cliente);
+    }
+
+    public void buscarClientes(View view) {
+        String nome = txtNome.getText().toString();
+        Intent intent = new Intent(this, ListaClientesActivity.class);
+        intent.putExtra(NOME, nome);
+        startActivity(intent);
+    }
+}
